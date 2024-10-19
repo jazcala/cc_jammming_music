5febd6c56c92f75aed8e526fcb78906dd9af6473
jazcala <jazcalabrese@gmail.com>
2024-10-18 23:24:00 -0300

    Implement adding and remove songs

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchResults.module.css';
 import Track from './Track';
 
 
 function SeachResults(props) {
     const results = props.results;
+    const addRemoveTrack = true;
+    const addPlaylist = props.addPlaylist;
     return (<>
 
         <div className={styles.searchResults}>
             <h2>Results</h2>
             {
                 results.map((result) => (
-                    <Track key={result.id} track={result} />
+                    <Track key={result.id} track={result} addRemoveTrack={addRemoveTrack} addPlaylist={addPlaylist} />
                 ))
 
             }
