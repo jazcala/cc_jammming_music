4a679032cd4b1249612b810318dab6c84957cf44
jazcala <jazcalabrese@gmail.com>
2024-12-05 14:59:58 -0300

    Addig soma basic tests to header, playlist, searchResults

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders Jamming Title', () => {
+
+it('renders Jamming Title', () => {
   render(<App />);
   const headerText = screen.getByText(/Jamming/i);
   expect(headerText).toBeInTheDocument();
 });
+
+
+it('user is allow to add title', async () => {
+  const user = userEvent.setup();
+  render(<App />);
+  const input = screen.getByRole('textbox', { name: 'playlist-title' })
+  const title = "Playlist Title";
+  await user.type(input, title);
+  expect(input).toHaveValue(title);
+
+})
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
-test('renders Jamming Title', () => {
+it('renders Jamming Title', () => {
   render(<Header />);
   const headerText = screen.getByText(/Jamming/i);
   expect(headerText).toBeInTheDocument();
