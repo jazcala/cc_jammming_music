acf1c2b840d8e3377dbba04dc926645e74c83d9c
jazcala <jazcalabrese@gmail.com>
2024-12-06 00:06:25 -0300

    Updates

--- src/components/Header.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Header from './Header';
-
-it('renders Jamming Title', () => {
-  render(<Header />);
-  const headerText = screen.getByText(/Jamming/i);
-  expect(headerText).toBeInTheDocument();
-});
--- src/components/Tracklist.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-it('', () => {
-
-})
-//TODO with mock data
